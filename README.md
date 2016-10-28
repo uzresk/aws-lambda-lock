ce488dae4a5b71ad624941f7dd3b30cb368fa950
uzresk <uzr.esk@gmail.com>
2016-10-28 17:14:42 +0900

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+aws lambda idempotent use DynamoDB
+==================================================
+
+Description
+-----
+For when the lambda is started with multiple, it is a library to acquire the lock only the specified number of milisecond.
+
+
+Author
+----------------------------
+[uzresk](https://twitter.com/uzresk)
---
+++ src/test/java/jp/gr/java_conf/uzresk/aws/lambda/LambdaLockTest.java
@@ -0,0 +1,29 @@
+package jp.gr.java_conf.uzresk.aws.lambda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.amazonaws.ClientConfiguration;
+
+@RunWith(JUnit4.class)
+public class LambdaLockTest {
+
+	@Test
+	public void locks() {
+		ClientConfiguration cc = new ClientConfiguration();
+		cc.setProxyHost("tkyproxy.intra.tis.co.jp");
+		cc.setProxyPort(8080);
+		// System.out.println(new LambdaLock(cc).lock("foo", 10, new
+		// TestContext()));
+	}
+
+	@Test
+	public void updateItem() {
+		ClientConfiguration cc = new ClientConfiguration();
+		cc.setProxyHost("tkyproxy.intra.tis.co.jp");
+		cc.setProxyPort(8080);
+		// new LambdaLock(cc).updateItem("foo", System.currentTimeMillis(), 10,
+		// new TestContext());
+	}
+}
